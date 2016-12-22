cfd40d4b6b2aaed523fc7cb6f0345499ca0368b2
Kevin <dragn194@gmail.com>
2016-12-21 22:34:45 -0500

    Refactor rankings rebuilding to be much faster
    rebuildrankings now builds lists of teams and matches first, and then
    uses bulk_create to massively speed up the rebuild.

--- VexELO_Web/VexELO_Web/VexELO_rankings/rankings/vexdb.py
+++ VexELO_Web/VexELO_Web/VexELO_rankings/rankings/vexdb.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 from VexELO_rankings.models import Match, Team
 
 class JsonMatch:
@@ -26,6 +27,7 @@ class VexDbApi:
 
     def get_all_matches(self):
         matches = list()
+        teams = dict()
         #get the number of current starstruck matches
         nodata_response = requests.get(self.MATCHES_URL, {'season':'current', 'nodata':True}).json()
         num_matches = nodata_response['size']
@@ -36,13 +38,18 @@ class VexDbApi:
             for team_json in data_response['result']:
                 #exclude VEX university matches
                 if "VEXU" not in team_json['sku']: 
-                    matches.append(self.parse_match_json(team_json))
+                    matches.append(self.parse_match_json(team_json, teams))
             count += data_response['size']
-        return matches
+        return matches, teams
         
-    def parse_match_json(self, json):
+    def parse_match_json(self, json, team_dict):
+        #determine which teams are actually playing
         redTeams = [json['red1'], json['red2'], json['red3']]
         blueTeams = [json['blue1'], json['blue2'], json['blue3']]
         redTeams = [x for x in redTeams if x != json['redsit']]
         blueTeams = [x for x in blueTeams if x != json['bluesit']]
+        #add teams in this match to the dict if they're not already there
+        for team_name in redTeams + blueTeams:
+            if team_name not in team_dict:
+                team_dict[team_name] = Team(name=team_name, elo=1500)
         return JsonMatch(redTeams[0], redTeams[1], blueTeams[0], blueTeams[1], json['redscore'], json['bluescore'])
