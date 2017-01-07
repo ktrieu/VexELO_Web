b1d0809dcd18e75aa571412cce2c68bfba757cf5
Kevin <dragn194@gmail.com>
2017-01-07 17:20:49 -0500

    Speed up rankings rebuild
    We now use grequests to asynchronously make requests while getting
    matches from VexDB

--- VexELO_Web/VexELO_Web/VexELO_rankings/rankings/vexdb.py
+++ VexELO_Web/VexELO_Web/VexELO_rankings/rankings/vexdb.py
@@ -2,6 +2,8 @@ import requests
 import json
 import dateutil.parser
 import datetime
+import collections
+import grequests
 from VexELO_rankings.models import Match, Team
 
 class VexDbApi:
@@ -25,20 +27,27 @@ class VexDbApi:
         events_json.sort(key=lambda k: dateutil.parser.parse(k['start']))
         matches = list()
         teams = dict()
+        request_urls = list()
         for event_json in events_json:
-            self.load_matches_from_event(event_json['sku'], dateutil.parser.parse(event_json['start']), matches, teams)
+            request_urls.append("{0}?sku={1}".format(self.MATCHES_URL, event_json['sku']))
+        requests_list = [grequests.get(u) for u in request_urls]
+        for idx, response in enumerate(grequests.map(requests_list, size=100, exception_handler=self.response_handler, stream=False)):
+            event_json = events_json[idx]
+            matches.extend(self.load_matches_from_event(response.json(), event_json['sku'], dateutil.parser.parse(event_json['start']), teams))
+            response.close()
         return matches, teams
 
-    def load_matches_from_event(self, sku, start_date, match_list, team_dict):
-        matches_response = requests.get(self.MATCHES_URL, {'sku':sku}).json()
+    def load_matches_from_event(self, matches_response, sku, start_date, team_dict):
         event_matches = matches_response['result']
+        loaded_matches = list()
         #sort the matches into their order at the event
         event_matches.sort(key=lambda k: k['matchnum'])
         event_matches.sort(key=lambda k: k['instance'])
         event_matches.sort(key=lambda k: k['round'])
         for match in event_matches:
             if match['scored'] == '1':
-                match_list.append(self.parse_match_json(match, sku, start_date, team_dict))
+                loaded_matches.append(self.parse_match_json(match, sku, start_date, team_dict))
+        return loaded_matches
 
     def parse_match_json(self, json, sku, start_date, team_dict):
         #determine which teams are actually playing
@@ -53,3 +62,7 @@ class VexDbApi:
         return Match(redTeam1=team_dict[redTeams[0]], redTeam2=team_dict[redTeams[1]],
                      blueTeam1=team_dict[blueTeams[0]], blueTeam2=team_dict[blueTeams[1]],
                      redScore=int(json['redscore']), blueScore=int(json['bluescore']), event_sku=sku, event_start_date=start_date)
+
+    def response_handler(self, request, exception):
+        print("Response exception.")
+        print(exception)
