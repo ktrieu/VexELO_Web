f8adcb4e250feddf449bf700d06effb1d07104a3
Kevin <dragn194@gmail.com>
2017-01-13 17:17:54 -0500

    Add match prediction api view

--- VexELO_Web/VexELO_Web/VexELO_rankings/urls.py
+++ VexELO_Web/VexELO_Web/VexELO_rankings/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.rankings, name="rankings"),    
-    url(r'^api/elo_data', views.elo_data, name='elo_data')
+    url(r'^api/elo_data', views.elo_data, name='elo_data'),
+    url(r'^api/predict_match', views.predict_match, name='predict_match')
 ]
--- VexELO_Web/VexELO_Web/VexELO_rankings/views.py
+++ VexELO_Web/VexELO_Web/VexELO_rankings/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.db import models
 from VexELO_rankings.models import Match, Team
+from VexELO_rankings.rankings import ranker
 
 def index(request):
     return HttpResponse("Testing django website.")
@@ -16,4 +17,15 @@ def elo_data(request):
     response_dict['data'] = list()
     for idx, team in enumerate(all_teams):
         response_dict['data'].append(dict({'name':team.name, 'elo':int(round(team.elo))})) 
+    return JsonResponse(response_dict)
+
+def predict_match(request):
+    redTeam1 = Team.objects.get(name__iexact=request.GET['redTeam1'])
+    redTeam2 = Team.objects.get(name__iexact=request.GET['redTeam2'])
+    blueTeam1 = Team.objects.get(name__iexact=request.GET['blueTeam1'])
+    blueTeam2 = Team.objects.get(name__iexact=request.GET['blueTeam2'])
+    red_chance, blue_chance = ranker.predict_match(redTeam1.elo, redTeam2.elo, blueTeam1.elo, blueTeam2.elo)
+    response_dict = dict()
+    response_dict['status'] = 1
+    response_dict['response'] = {'redProb': round(red_chance * 100, ndigits=2), 'blueProb': round(blue_chance * 100, ndigits=2)}
     return JsonResponse(response_dict)
