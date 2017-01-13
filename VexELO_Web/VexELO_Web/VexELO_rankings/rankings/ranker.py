89c619be3a2ccc6eba0d657847b4c2ea63e8b06c
Kevin <dragn194@gmail.com>
2017-01-12 23:34:38 -0500

    Refactor ranker.py
    Predicting the outcome of a match and actually ranking one are now
    separate functions

--- VexELO_Web/VexELO_Web/VexELO_rankings/rankings/ranker.py
+++ VexELO_Web/VexELO_Web/VexELO_rankings/rankings/ranker.py
@@ -2,15 +2,20 @@ import math
 
 K_FACTOR = 32
 
-def rank_match(match, team_dict):
-    red_elo = calc_alliance_elo(team_dict, match.redTeam1, match.redTeam2)
-    blue_elo = calc_alliance_elo(team_dict, match.blueTeam1, match.blueTeam2)
+
+def predict_match(red_elo1, red_elo2, blue_elo1, blue_elo2):
+    red_elo = calc_alliance_elo(red_elo1, red_elo2)
+    blue_elo = calc_alliance_elo(blue_elo1, blue_elo2)
     #transform elos according to the elo formula
     red_elo = math.pow(10, red_elo / 400)
     blue_elo = math.pow(10, blue_elo / 400)
     #get expected results
     red_expected = red_elo / (red_elo + blue_elo)
     blue_expected = blue_elo / (red_elo + blue_elo)
+    return red_expected, blue_expected
+    
+def rank_match(match, team_dict):
+    red_expected, blue_expected = predict_match(match.redTeam1.elo, match.redTeam2.elo, match.blueTeam1.elo, match.blueTeam2.elo)
     red_actual = 0
     blue_actual = 0
     if match.redScore > match.blueScore:
@@ -30,7 +35,7 @@ def rank_match(match, team_dict):
     blue_change = margin_adjust * K_FACTOR * (blue_actual - blue_expected)
     apply_elo(team_dict, match.redTeam1, match.redTeam2, red_change)
     apply_elo(team_dict, match.blueTeam1, match.blueTeam2, blue_change)
-    
+
 def calc_margin_adjust(red_score, blue_score):
     margin = math.fabs(red_score - blue_score)
     if margin == 0:
@@ -38,8 +43,8 @@ def calc_margin_adjust(red_score, blue_score):
     else:
         return math.log(margin, 10) + 1
 
-def calc_alliance_elo(team_dict, team1, team2):
-    return (team1.elo + team2.elo) / 2
+def calc_alliance_elo(team_elo1, team_elo2):
+    return (team_elo1, team_elo2) / 2
 
 def apply_elo(team_dict, team1, team2, change):
     contrib1 = team1.elo / (team1.elo + team2.elo)
