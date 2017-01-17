403fb29820363f84bf29049bc00d72dbff9c031b
Kevin <dragn194@gmail.com>
2017-01-16 21:16:57 -0500

    Add another dir for static files

--- VexELO_Web/VexELO_Web/VexELO_Web/settings.py
+++ VexELO_Web/VexELO_Web/VexELO_Web/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
