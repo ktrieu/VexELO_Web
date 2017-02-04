cd3012a91c4b08f734546fa80f745ca21978710e
betato <battlestargatticus@gmail.com>
2017-02-04 11:00:18 -0500

    Fix str cast issue

--- VexELO_Web/VexELO_Web/VexELO_rankings/management/commands/rebuildrankings.py
+++ VexELO_Web/VexELO_Web/VexELO_rankings/management/commands/rebuildrankings.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
         vex_db_api = VexDbApi()
         self.stdout.write("Getting matches and teams...")
         matches, teams = vex_db_api.get_matches_and_teams()
-        self.stdout.write(len(teams.values()))
+        self.stdout.write(str(len(teams.values())))
         self.stdout.write(teams.values())
         #rank teams for each match
         self.stdout.write("Ranking matches...")
